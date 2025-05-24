cf37794ca36ad6f341f063b546a7ae63300b155f
Tomas Pavlik <tomas.pavlik2237@seznam.cz>
2025-05-24 13:44:29 +0200

    Test seriaze field

--- Runtime/DefaultData.cs
+++ Runtime/DefaultData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Other.Template {
     public class DefaultData : MonoBehaviour {
         
-        public int Mass = 1000;
+        [SerializeField] public int Mass = 1000;
         public Mesh Mesh;
         public int NumOfMat = 1;
         public Material[] Materials;
