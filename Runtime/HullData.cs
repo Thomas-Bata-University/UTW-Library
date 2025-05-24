048cabc3d6e674a90b289fd20ab9e38438726ec3
Tomas Pavlik <tomas.pavlik2237@seznam.cz>
2025-05-24 14:12:57 +0200

    Change data test

--- Runtime/HullData.cs
+++ Runtime/HullData.cs
@@ -1,8 +1,16 @@
 using System;
+using UnityEngine;
 
 namespace Other.Template {
     [Serializable]
-    public class HullData : DefaultData {
+    public class HullData : MonoBehaviour {
+
+        public int Mass = 1000;
+        public Mesh Mesh;
+        public int NumOfMat = 1;
+        public Material[] Materials;
+        public int NumOfCol = 1;
+        public Mesh[] Colliders;
 
         public bool showPreview = true;
         public float trackOffset = 1f;
